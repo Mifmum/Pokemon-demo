a0ba2aeb0871abd9fae7d897d7db3054077db154
JustinBeaudry <justin.beaudry+github@creativelive.com>
2015-05-28 17:17:42 -0700

    adds pokemon.json file and a placeholder for a pokemon page generator

---
+++ bin/pokemonGenerator.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var args = require('minimist')(process.argv.slice(2));
+var os = require('osenv');
+var fs = require('graceful-fs');
+
+var pokekon = require('../pokemon.json');
+
+console.info('TODO');
